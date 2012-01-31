6c624058c34caf3cc4c4ef71302fd19a851a4c35
Truls <truls@truls-ThinkPad-SL300.(none)>
2012-01-31 20:42:29 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'ba9b993c6c2caeadb02b176a7d354ae701e2b2f5f39455ab27ddca21268ca1cb35c74a0bc9beeed5ab76b85c81ceecab68ec6e4c4568aa3c57eb80aee2e0c285'
